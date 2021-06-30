66e3085a0b26feb26aa2cdb036a46f3a31eb7c06
Huey205 <86713354+Huey205@users.noreply.github.com>
2021-06-30 16:18:04 +0700

    Update config.js

--- config.js
+++ config.js
@@ -8,12 +8,12 @@ const CONFIG = {
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai mưa à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
+    btnNo: 'Không, em chỉ xem anh là bạn :3',
+    question:'HiHi, yêu quá à. Mà tại sao bé rung động với mỗi anh trong khi đó có hơn 7 tỉ người trên thế gian này?? <3',
+    btnReply: 'Tâm sự với anh tí <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tí 6h anh call cho em nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    mess: 'Anh biết mà 🥰. Yêu em vailonluon 😘😘',
+    messDesc: 'Tí tới chơi em nha:)).',
+    btnAccept: 'Dạ <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
