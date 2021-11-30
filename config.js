0530385bfb2c6650ef5ad00c29004bf51d6d8ec4
Huey205 <86713354+Huey205@users.noreply.github.com>
2021-11-30 08:18:09 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Bae à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Anh bạn à!',
+    introDesc: `Long là người như nào`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, em chỉ xem anh là bạn :3',
-    question:'HiHi, yêu quá à. Mà tại sao bé rung động với mỗi anh trong khi đó có hơn 7 tỉ người trên thế gian này?? <3',
-    btnReply: 'Tâm sự với anh tí <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em vailonluon 😘😘',
-    messDesc: 'Tí tới chơi em nha:)).',
-    btnAccept: 'Dạ <3',
+    title: '1 số cảm nghĩ của bạn ',
+    desc: 'về long ',
+    btnYes: 'chó long',
+    btnNo: 'long đẹp trai:))',
+    question:'Tại sao lại nhận xét long như thế :3',
+    btnReply: 'Tâm sự với đại ca tí :3',
+    reply: 'Long hãm lòn vl ra haha',
+    mess: 'đại ca biết mà 🥰. Long ngu vailonluon 😘😘',
+    messDesc: 'chiều thể dục em nhá:)).',
+    btnAccept: 'ok đại ca',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
